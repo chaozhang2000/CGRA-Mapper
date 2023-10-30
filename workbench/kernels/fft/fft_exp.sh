265de3467fbd334403450a62e7c158797e396878
zhangchao <chaozhang2000@outlook.com>
2023-10-31 07:37:38 +0800

    跑完了mvt测试，跑fft时遇到了问题，准备开始修改

---
+++ workbench/kernels/fft/fft_exp.sh
@@ -0,0 +1,9 @@
+mkdir -p output
+filename=$1
+while IFS= read -r line
+do
+./gen_param $line 4 4 > paramCGRA.json 2>./output/$line.txt
+bash run.sh
+done < $filename
+
+#./gen_param $1 4 4 > paramCGRA.json 2>./output/$1.txt
--- workbench/kernels/fft/gdb.sh
+++ workbench/kernels/fft/gdb.sh
@@ -1,1 +1,7 @@
+cd ../..
+make cleanall
+make mappergdb
+cd kernels/fft
+#./gen_param 0xffff 4 4 > paramCGRA.json
 gdb opt-12 -x script.gdb
+
