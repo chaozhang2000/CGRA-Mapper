93227b6983055df0bfa8bacec324ee191db421df
zhangchao <chaozhang2000@outlook.com>
2023-11-02 15:43:16 +0800

    实验1形状实验完成

---
+++ workbench/kernels/mvt/tmp.sh
@@ -0,0 +1,10 @@
+mkdir -p output
+filename=$1
+while IFS= read -r line
+do
+./gen_param $line 6 4 > paramCGRA.json 2>./output/$line.txt
+#bash gdb1.sh
+opt-12 -load ../../../build/src/libmapperPass.so -mapperPass kernel.bc
+done < $filename
+
+#./gen_param $1 4 4 > paramCGRA.json 2>./output/$1.txt
